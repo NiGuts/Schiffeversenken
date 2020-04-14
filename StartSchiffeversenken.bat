4f18a66658ba84c229b4118c4d810c9ac458c777
NiGuts <56599517+NiGuts@users.noreply.github.com>
2020-04-14 20:26:41 +0200

    Update StartSchiffeversenken.bat

--- StartSchiffeversenken.bat
+++ StartSchiffeversenken.bat
@@ -1,2 +1,2 @@
 cd Schiffeversenken\out\production\Schiffeversenken2.0
-"..\..\..\..\Java\jdk-11.0.2\bin\java.exe"  --module-path "Pfad\zum\lib\Ordner\meiner\JavaFX\Installation\lib" --add-modules javafx.controls,javafx.fxml battleships.GUI.Main
+java  --module-path "Pfad\zum\lib\Ordner\meiner\JavaFX\Installation\lib" --add-modules javafx.controls,javafx.fxml battleships.GUI.Main
